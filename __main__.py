88d7be20fee37e779cb7bfadd1bc63bbc9fc1f8d
Deyvid Andrade <deyvid-asilva@hotmail.com>
2022-06-20 08:52:37 -0300

    + Abrir imagem apartir de um url

--- __main__.py
+++ __main__.py
@@ -1,9 +1,13 @@
 """Main module"""
+import os
 import sys
+from urllib.request import urlretrieve
 
 from PyQt6.QtWidgets import QApplication
 
 from src.app import DoImageViewer
+
+CAMINHO_RES = os.path.join(f'{os.getcwd()}/src/res/tmp.jpg')
 
 
 def main() -> None:
@@ -15,15 +19,26 @@ def main() -> None:
         caminho_arquivo = sys.argv[1]
     except IndexError:
         caminho_arquivo = ""
-    # verificar o tipo de uri
 
+    # verificar o tipo de uri
+    is_url = False
     if caminho_arquivo.find("http") != -1:
-        pass
+        urlretrieve(caminho_arquivo, CAMINHO_RES)
+        caminho_arquivo = CAMINHO_RES
+        is_url = True
 
     app = QApplication(sys.argv)
+
+    # noinspection PyUnresolvedReferences
+    app.aboutToQuit.connect(lambda: on_exit(is_url))
     window = DoImageViewer(app, caminho_arquivo)
     window.show()
     sys.exit(app.exec())
+
+
+def on_exit(is_url: bool):
+    if is_url:
+        os.remove(CAMINHO_RES)
 
 
 if __name__ == '__main__':
