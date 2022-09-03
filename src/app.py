793f8bba233447b8e3fd01bd2e6b2af88b67411c
deyvidandradees <deyvid-asilva@hotmail.com>
2022-09-03 20:33:38 -0300

    * Reorganização do cógigo da GUI

--- src/app.py
+++ src/app.py
@@ -100,21 +100,24 @@ class DoImageViewer(QMainWindow):
             self.__carregar_imagem()
 
     def __configurar_gui(self):
+
+        # Labels para mudar a imagem
         self.label_left = QLabelClick()
         self.label_left.setPixmap(QPixmap(self.__RESOURCES + 'left-svgrepo-com.svg').scaled(self.tamanho_icones))
         self.label_left.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.label_left.clicked.connect(lambda: self.__mudar_imagem('esq'))
         self.label_left.setHidden(True)
-        self.__layout_principal.addWidget(self.label_left)
-
-        self.__layout_principal.addWidget(self.__viewer, 1)
 
         self.label_right = QLabelClick()
         self.label_right.setPixmap(QPixmap(self.__RESOURCES + 'right-svgrepo-com.svg').scaled(self.tamanho_icones))
         self.label_right.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.label_right.clicked.connect(lambda: self.__mudar_imagem('dir'))
         self.label_right.setHidden(True)
-        self.__layout_principal.addWidget(self.label_right)
+
+        # Configuração do layout
+        self.__layout_principal.addLayout(self.label_left)
+        self.__layout_principal.addWidget(self.__viewer, 1)
+        self.__layout_principal.addLayout(self.label_right)
 
     # noinspection PyUnresolvedReferences
     def __configurar_menu(self):
@@ -344,9 +347,13 @@ class DoImageViewer(QMainWindow):
                 if len(lista_final) <= 1:
                     self.label_left.setHidden(True)
                     self.label_right.setHidden(True)
+                    self.label_girar_dir.setHidden(True)
+                    self.label_girar_esq.setHidden(True)
                 else:
                     self.label_left.setHidden(False)
                     self.label_right.setHidden(False)
+                    self.label_girar_dir.setHidden(False)
+                    self.label_girar_esq.setHidden(False)
 
                 # atualizando variaveis
                 self.__info_dir = {
